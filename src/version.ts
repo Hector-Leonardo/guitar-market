26711ad07e38ea0288578c3cfdf1481cbfd50555
Hector <pichis523@gmail.com>
2026-05-04 21:14:10 -0600

    Add version marker and console log

---
+++ src/version.ts
@@ -0,0 +1,4 @@
+// Version check file - Last updated: 2024-05-05 03:15
+// If you see this in the browser console, Vercel is serving the latest code
+export const VERSION = "v5.0-production-ready";
+console.log("🎉 Guitar Market Payment System - Version:", VERSION);
