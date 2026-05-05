ddc1d57312e011845e4332d43ee0c1715cfca887
Hector <pichis523@gmail.com>
2026-05-04 20:51:56 -0600

    Remove backend folders - client-side MP integration only

--- server/routes/cloudinary.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { deleteImage } from '../controllers/cloudinary.controller.js'
-
-const router = Router()
-
-// Ruta para eliminar imagen
-router.delete('/delete', deleteImage)
-
-export default router
